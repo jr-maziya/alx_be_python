6c826b584ca4dad79506183d25bdd37c8c548b2d
Geoff Maziya <geoffmaziya04@gmail.com>
2024-06-20 14:40:56 +0200

    savings calculator variable expense change to expenses

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = input("Enter your monthly income: ")
-monthly_expense = input("Enter your total monthly expenses: ")
-monthly_savings = float(monthly_income) - float(monthly_expense)
+monthly_expenses = input("Enter your total monthly expenses: ")
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 projected_savings = monthly_savings * 12 + (float(monthly_savings) * 12 * 0.05)
 print("Your monthly savings are ",monthly_savings)
 print("Projected savings after one year, with interest, is: ",projected_savings)
