54f72a8ba5c5c7efce37b2dbd89cb31ce2529215
Geoff Maziya <geoffmaziya04@gmail.com>
2024-06-20 14:38:32 +0200

    savings calculator variable types change_2

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,8 @@
-monthly_income = float(input("Enter your monthly income: "))
-monthly_expense = float(input("Enter your total monthly expenses: "))
-monthly_savings = monthly_income - monthly_expense
+monthly_income = input("Enter your monthly income: ")
+monthly_expense = input("Enter your total monthly expenses: ")
+monthly_savings = float(monthly_income) - float(monthly_expense)
 projected_savings = monthly_savings * 12 + (float(monthly_savings) * 12 * 0.05)
 print("Your monthly savings are ",monthly_savings)
 print("Projected savings after one year, with interest, is: ",projected_savings)
+
+#monthly_savings = (monthly_income - monthly_expenses|float(monthly_income) - float(monthly_expenses))
