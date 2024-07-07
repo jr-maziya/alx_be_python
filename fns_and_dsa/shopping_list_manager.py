1cb0a9b3fa73a3628b7eceb4a30d5f7651c2dd64
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-07 08:18:44 +0200

    shopping list pg first commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_item = input("Enter the item name you'd like to add the list: ")
+            shopping_list.append(add_item)
+        elif choice == '2':
+            remove_item = input("Enter the item you would like to remove: ")
+            if remove_item in shopping_list:
+                shopping_list.remove(remove_item)
+            else:
+                print("Item not in shopping list")
+        elif choice == '3':
+            print()
+            print("Below are the items in your shopping list:")
+            for item in shopping_list:
+                print(item)
+            print()
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
