74989d28b67f4afa4e599c3704b6450adcdae875
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-07 09:41:58 +0200

    Addition of formatted dates with strftime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,14 +3,16 @@ from datetime import datetime, timedelta
 def display_current_datetime():
     global current_date
     current_date = datetime.now()
-    print(f"Current date and time: {current_date}")
+    formated_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formated_date}")
 
 
 number_of_days = int((input("Enter the number of days to add to the current date: ")))
 
 def calculate_future_date():
     future_date = current_date + timedelta(number_of_days)
-    print(f"Future date: {future_date}") 
+    formated_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Future date: {formated_date}") 
 
 
 display_current_datetime()
