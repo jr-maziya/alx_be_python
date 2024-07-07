aa6c1574185f3c6a4e47a3c0d123a4838241c057
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-07 07:28:47 +0200

    arithmetic operations program

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        result = num1 / num2
+        if num2 == 0:
+            print("Cannot divide by 0")
+            
+    return result
