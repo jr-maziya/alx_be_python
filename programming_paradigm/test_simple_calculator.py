876cdeae660e5ac5771b32238ec6c856ac3fe008
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-14 19:06:42 +0200

    changes to all 3 method

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,11 +8,11 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(2,3),5)
     def test_subtraction(self):
-        self.assertEqual(self.calc.add(6,3),3)
+        self.assertEqual(self.calc.subtract(6,3),3)
     def test_multiply(self):
-        self.assertEqual(self.calc.add(2,3),6)
+        self.assertEqual(self.calc.multiply(2,3),6)
     def test_divide(self):
-        self.assertEqual(self.calc.add(10,5),2)
+        self.assertEqual(self.calc.divide(10,5),2)
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(10, 0)
