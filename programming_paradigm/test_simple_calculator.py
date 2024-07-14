7c65abd1c320b0e95a20280811d3a40c559b6270
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-14 19:07:40 +0200

    changes to multiplication and division methods naming

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,9 +9,9 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2,3),5)
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(6,3),3)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,3),6)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,5),2)
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
