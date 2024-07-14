a5bac1eae4428e85e25a3b0e579fc696a267935b
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-14 19:05:44 +0200

    changes to subtraction method

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_addition(self):
         self.assertEqual(self.calc.add(2,3),5)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.add(6,3),3)
     def test_multiply(self):
         self.assertEqual(self.calc.add(2,3),6)
