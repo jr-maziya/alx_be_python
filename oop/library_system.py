1fe7155b173597a40a8f7d43bbeba2161398e7dc
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-21 19:39:03 +0200

    changes to return statements for EBook and PrintBook

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.title} by {self.author}, File size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}MB"
 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count : int):
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{self.title} by {self.author}, Page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
 
 class Library:
     def __init__(self, books = None):
