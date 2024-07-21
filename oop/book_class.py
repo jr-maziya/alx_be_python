f601b82e3c5e2f74727d333daacbe25c81fd7607
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-21 18:58:23 +0200

    changes to attributes definition

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,14 @@
 class Book:
     def __init__(self, title : str, author : str, year : int):
-        self.book_title = title
-        self.book_author = author
-        self.book_year = year
+        self.title = title
+        self.author = author
+        self.year = year
 
     def __del__(self):
-        print(f"Deleting {self.book_title}")
+        print(f"Deleting {self.title}")
     
     def __str__(self):
-        print (f"{self.book_title} by {self.book_author}, published in {self.book_year}")
+        print (f"{self.title} by {self.author}, published in {self.year}")
 
     def __repr__(self):
-        print(f"Book('{self.book_title}', '{self.book_author}', {self.book_year})")
+        print(f"Book('{self.title}', '{self.author}', {self.year})")
